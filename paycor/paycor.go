69e197f61cf84f4c0049df412a63e50a1499826e
joerust <joseph.rust@53.com>
2016-10-05 15:30:33 -0400

    Add files via upload

--- paycor/paycor.go
+++ paycor/paycor.go
@@ -17,8 +17,10 @@ import (
 	"errors"
 	"fmt"
     "encoding/json"
+	"reflect"
+	"unsafe"
+	"strings"
 	"github.com/hyperledger/fabric/core/chaincode/shim"
-	"github.com/joerust/referral-partners/partnerlogic"
 )
 
 type PartnerChaincode struct {
@@ -42,6 +44,154 @@ func main() {
 	if err != nil {
 		fmt.Printf("Error starting Simple chaincode: %s", err)
 	}	
+}
+
+func BytesToString(b []byte) string {
+    bh := (*reflect.SliceHeader)(unsafe.Pointer(&b))
+    sh := reflect.StringHeader{bh.Data, bh.Len}
+    return *(*string)(unsafe.Pointer(&sh))
+}
+
+func RemoveStatusReferralIndex(referralId string, status string, stub *shim.ChaincodeStub) (error) {
+	valAsbytes, err := stub.GetState(status)
+	if err != nil {
+		jsonResp := "{\"Error\":\"Failed to get state for " + status + "\"}"
+		return errors.New(jsonResp)
+	}
+	
+	if valAsbytes == nil {
+		return nil;
+	} else {
+		// Remove the referral from this status type, if it exists
+		commaDelimitedStatuses := BytesToString(valAsbytes)
+		referralIdsInCurrentStatus := strings.Split(commaDelimitedStatuses, ",")
+		updatedReferralIdList := ""
+		
+		appendComma := false
+		for i := range referralIdsInCurrentStatus {
+			if referralIdsInCurrentStatus[i] != referralId {
+			    if appendComma == false {
+					updatedReferralIdList += referralIdsInCurrentStatus[i]
+					appendComma = true
+				} else {
+					updatedReferralIdList = updatedReferralIdList + "," + referralIdsInCurrentStatus[i]
+				}
+			}
+		}
+		
+		err = stub.PutState(status, []byte(updatedReferralIdList))
+	}
+	
+	if err != nil {
+		jsonResp := "{\"Error\":\"Failed to update state for " + status + "\"}"
+		return errors.New(jsonResp)
+	}
+	
+	return nil
+}
+
+// Adds the referral id to a ledger list item for the given department allowing for quick search of referrals in a given department
+func IndexByStatus(referralId string, status string, stub *shim.ChaincodeStub) (error) {
+	valAsbytes, err := stub.GetState(status)
+	if err != nil {
+		jsonResp := "{\"Error\":\"Failed to get state for " + status + "\"}"
+		return errors.New(jsonResp)
+	}
+	
+	if valAsbytes == nil {
+		err = stub.PutState(status, []byte(referralId))
+	} else {
+	    commaDelimitedStatuses := BytesToString(valAsbytes)
+		err = stub.PutState(status, []byte(commaDelimitedStatuses + "," + referralId))
+	}
+	
+	if err != nil {
+		jsonResp := "{\"Error\":\"Failed to update state for " + status + "\"}"
+		return errors.New(jsonResp)
+	}
+	
+	return nil
+}
+
+func ProcessCommaDelimitedReferrals(delimitedReferrals string, stub *shim.ChaincodeStub) ([]byte, error) {
+	commaDelimitedReferrals := strings.Split(delimitedReferrals, ",")
+
+	referralResultSet := "["
+	
+	for i := range commaDelimitedReferrals {
+		valAsbytes, err := stub.GetState(commaDelimitedReferrals[i])
+		
+		if err != nil {
+			return nil, err
+		}
+		
+		if i == 0 {
+			referralResultSet = referralResultSet + BytesToString(valAsbytes)
+		} else {
+			referralResultSet = referralResultSet + "," + BytesToString(valAsbytes)
+		}
+	}
+	
+	referralResultSet += "]"
+	return []byte(referralResultSet), nil
+}
+
+func FindAllReferrals(stub *shim.ChaincodeStub, partnerName string) ([]byte, error) {
+	valAsbytes, err := stub.GetState(partnerName)
+	
+	if err != nil {
+		jsonResp := "{\"Error\":\"Failed to get state for " + partnerName + "\"}"
+		return nil, errors.New(jsonResp)
+	}
+	
+	valAsbytes, err = ProcessCommaDelimitedReferrals(BytesToString(valAsbytes), stub)
+	
+	if(err != nil) {
+		return nil, err
+	}
+	
+	return valAsbytes, nil
+}
+
+func SearchByStatus(status string, stub *shim.ChaincodeStub) ([]byte, error) {
+	valAsbytes, err := stub.GetState(status)
+	
+	if err != nil {
+		jsonResp := "{\"Error\":\"Failed to get state for " + status + "\"}"
+		return nil, errors.New(jsonResp)
+	}
+	
+	valAsbytes, err = ProcessCommaDelimitedReferrals(BytesToString(valAsbytes), stub)
+	
+	if(err != nil) {
+		return nil, err
+	}
+	
+	return valAsbytes, nil
+}
+
+
+// read - query function to read key/value pair
+func Read(stub *shim.ChaincodeStub, args []string) ([]byte, error) {
+	var key, jsonResp string
+	var err error
+	
+	if len(args) != 1 {
+		return nil, errors.New("Incorrect number of arguments. Expecting name of the key to query")
+	}
+
+	key = args[0]
+	valAsbytes, err := stub.GetState(key)
+	
+	if err != nil {
+		jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
+		return []byte(jsonResp), err
+	}
+	
+	if valAsbytes == nil {
+		return []byte("Did not find entry for key: " + key), nil
+	}
+	return valAsbytes, nil
 }
 
 // Init resets all the things
@@ -124,14 +274,14 @@ func (t *PartnerChaincode) updateReferralStatus(stub *shim.ChaincodeStub, args [
 	}
 	
 	// Index things by the new status
-	err = partnerlogic.IndexByStatus(key, referral.Status, stub)
+	err = IndexByStatus(key, referral.Status, stub)
 	
 	if err != nil {
 		return []byte("Count not index the bytes by status from the value: " + value + " on the ledger"), err
 	}
 	
 	// Remove the indexing by the status before the update
-	err = partnerlogic.RemoveStatusReferralIndex(key, oldStatus, stub)
+	err = RemoveStatusReferralIndex(key, oldStatus, stub)
 	
 	return valAsbytes, nil
 }
@@ -161,7 +311,7 @@ func (t *PartnerChaincode) createReferral(stub *shim.ChaincodeStub, args []strin
 
 	
 	// Deserialize the input string into a GO data structure to hold the referral
-	err = partnerlogic.IndexByStatus(referralKey, referral.Status, stub)
+	err = IndexByStatus(referralKey, referral.Status, stub)
 	
 	if err != nil {
 		return []byte("Count not index the bytes by status from the value: " + referralData + " on the ledger"), err
@@ -178,7 +328,7 @@ func (t *PartnerChaincode) searchByStatus(status string, stub *shim.ChaincodeStu
 		return nil, errors.New(jsonResp)
 	}
 	
-	valAsbytes, err = partnerlogic.ProcessCommaDelimitedReferrals(partnerlogic.BytesToString(valAsbytes), stub)
+	valAsbytes, err = ProcessCommaDelimitedReferrals(BytesToString(valAsbytes), stub)
 	
 	if(err != nil) {
 		return nil, err
